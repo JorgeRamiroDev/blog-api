dd7e1145269adf2640a5d60d4cfd7c3d1273812a
Jorge Ramiro <102836967+JorgeRamiroDev@users.noreply.github.com>
2023-11-24 00:33:44 -0300

    Cadastro app blog setings

--- Blog/settings.py
+++ Blog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #Meus Apps
+    "apps.blog",
+    #Ferramentas
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
