3d5909ebf09869ddc622459747ec03fba9499bb4
Jorge Ramiro <102836967+JorgeRamiroDev@users.noreply.github.com>
2023-11-24 00:42:14 -0300

    Criação models,admin

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    autor = models.CharField(max_length=150,null=True,blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.autor:
+            self.autor = "Anônimo"
+        super().save(*args, **kwargs)   
+
+    def __str__(self):
+        return self.titulo
